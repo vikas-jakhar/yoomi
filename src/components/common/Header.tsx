3d380629c3e2fbee724a24687213b03d2c273cd1
vikas-jakhar <vikashjakhar2004@gmail.com>
2024-09-28 09:52:19 +0530

    navbar update

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -44,15 +44,15 @@ const Header: React.FC<HeaderProps> = ({ toggleFormVisibility }) => {
                             priority
                         />
                     </Link>
-                    <div className={`${open ? "left-0 bg-white" : "-left-full"} flex items-center z-10 md:bg-transparent absolute flex-col w-full h-screen justify-center gap-7 top-0 md:relative md:bg-none md:flex-row md:gap-11 md:h-fit md:w-fit md:left-auto transition_slow`}>
+                    <div className={`${open ? "left-0 bg-white" : "-left-full"} flex items-center z-30 md:bg-transparent fixed flex-col w-full h-screen justify-center gap-7 top-0 md:relative md:bg-none md:flex-row md:gap-11 md:h-fit md:w-fit md:left-auto transition_slow`}>
                         {HEADER_DATA.map((item, index) => (
                             <Link key={index} href={item.url} aria-label={item.text} onClick={ToggleOpen} className="font-medium navBar text-lg transition_slow text-black w-fit relative after:absolute after:duration-300 after:ease-linear after:-bottom-[3px] after:left-1/2 after:-translate-x-1/2 after:bg-black after:w-0 after:h-[3px] after:rounded-xl hover:after:w-full">
                                 {item.text}
                             </Link>
                         ))}
-                        <PrimaryButton text="Get Started" onClick={toggleFormVisibility} /> {/* Call the function to show the form */}
+                        <PrimaryButton text="Get Started" onClick={toggleFormVisibility} />
                     </div>
-                    <div role="button" aria-label="menu icon" aria-expanded={open} onClick={ToggleOpen} className={`${open ? "w-9" : "w-7"} h-5 relative overflow-hidden md:hidden flex z-20 cursor-pointer transition_slow`}>
+                    <div role="button" aria-label="menu icon" aria-expanded={open} onClick={ToggleOpen} className={`${open ? "w-9" : "w-7"} h-5 relative overflow-hidden md:hidden flex z-40 cursor-pointer transition_slow`}>
                         <span className={`${open ? "opacity-0" : "top-0 right-0"} absolute w-full h-1 rounded-md bg-black transition_slow`}></span>
                         <span className={`${open ? "opacity-0" : "opacity-100 right-0"} top-1/2 -translate-y-1/2 absolute w-full h-1 rounded-md bg-black transition_slow`}></span>
                         <span className={`${open ? "after:-top-[18px]" : "after:top-2"} after:content-['Close'] after:font-inter after:font-bold text-xs after:absolute bottom-0 right-0 absolute w-full h-1 rounded-md bg-black after:duration-300 after:ease-linear`}></span>
