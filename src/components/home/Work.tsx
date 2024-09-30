d5eba434b5e148013eb7709c0322da9c7765fdfb
vikas-jakhar <vikashjakhar2004@gmail.com>
2024-09-30 12:30:54 +0530

    work sextion image update

--- src/components/home/Work.tsx
+++ src/components/home/Work.tsx
@@ -4,7 +4,8 @@ const Work = () => {
     return (
         <div className='lg:-mt-32 xl:-mt-40 md:-mt-28 sm:-mt-24 -mt-16 relative z-10 2xl:-mt-28'>
             <div className="container">
-                <Image src='/assets/images/webp/work-img.webp' className='w-full pointer-events-none' width={2367} height={1430} alt='work' priority />
+                <Image src='/assets/images/webp/work-img.webp' className='w-full hidden md:flex pointer-events-none' width={2367} height={1430} alt='work' priority />
+                <Image src='/assets/images/webp/work-img-small.webp' className='w-full md:hidden pointer-events-none' width={2367} height={1430} alt='work' priority />
             </div>
         </div>
     )
