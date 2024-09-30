e3d9668230c1a126b0f00215540520c3c39d4453
vikas-jakhar <vikashjakhar2004@gmail.com>
2024-09-30 12:38:34 +0530

    main section update

--- src/components/home/MainLayout.tsx
+++ src/components/home/MainLayout.tsx
@@ -1,9 +1,8 @@
 "use client";
 import React, { useEffect, useRef, useState } from 'react';
+import Header from '../common/Header';
+import Hero from './Hero';
 import Image from 'next/image';
-import dynamic from 'next/dynamic';
-const DynamicHeader = dynamic(() => import('../common/Header'), { ssr: false });
-const DynamicHero = dynamic(() => import('./Hero'), { ssr: false });
 
 const MainLayout: React.FC = () => {
     const [isFormVisible, setFormVisible] = useState(false);
@@ -28,8 +27,8 @@ const MainLayout: React.FC = () => {
 
     return (
         <div className='sm:bg-hero-layer bg-hero-small bg-cover sm:pb-20 lg:min-h-[828px] bg-center max-w-[1920px] mx-auto relative'>
-            <DynamicHeader toggleFormVisibility={toggleFormVisibility} />
-            <DynamicHero isFormVisible={isFormVisible} setFormVisible={setFormVisible} formRef={formRef} />
+            <Header toggleFormVisibility={toggleFormVisibility} />
+            <Hero isFormVisible={isFormVisible} setFormVisible={setFormVisible} formRef={formRef} />
             <Image src='/assets/images/webp/work-top-layer.webp' className='w-full hidden sm:flex pointer-events-none absolute lg:max-w-[574px] md:max-w-[420px] sm:max-w-[350px] max-w-[200px] left-1/2 -translate-x-1/2 bottom-10 sm:bottom-14 xl:bottom-[68px] 2xl:bottom-5' width={574} height={84} alt='layer' priority />
             <Image src='/assets/images/webp/work-small-layer.webp' className='w-full sm:hidden pointer-events-none absolute lg:max-w-[574px] md:max-w-[420px] sm:max-w-[350px] max-w-[200px] left-1/2 -translate-x-1/2 bottom-10 sm:bottom-14 xl:bottom-[68px] 2xl:bottom-5' width={574} height={84} alt='layer' priority />
         </div>
